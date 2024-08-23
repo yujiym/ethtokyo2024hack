8fc7fa974634cdba54ea413f2e81d1edbf10ba8c
Yuji Yamaguchi <yuji.ym@gmail.com>
2024-08-23 19:50:58 +0900

    fix config

--- packages/web/src/wagmi.ts
+++ packages/web/src/wagmi.ts
@@ -1,28 +1,24 @@
-import { http, cookieStorage, createConfig, createStorage } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
-import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
+import { http, cookieStorage, createConfig, createStorage } from "wagmi";
+import { sepolia, scrollSepolia } from "wagmi/chains";
+import { coinbaseWallet } from "wagmi/connectors";
 
 export function getConfig() {
   return createConfig({
-    chains: [mainnet, sepolia],
-    connectors: [
-      injected(),
-      coinbaseWallet(),
-      walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID }),
-    ],
+    chains: [sepolia, scrollSepolia],
+    connectors: [coinbaseWallet()],
     storage: createStorage({
       storage: cookieStorage,
     }),
     ssr: true,
     transports: {
-      [mainnet.id]: http(),
       [sepolia.id]: http(),
+      [scrollSepolia.id]: http(),
     },
-  })
+  });
 }
 
-declare module 'wagmi' {
+declare module "wagmi" {
   interface Register {
-    config: ReturnType<typeof getConfig>
+    config: ReturnType<typeof getConfig>;
   }
 }
